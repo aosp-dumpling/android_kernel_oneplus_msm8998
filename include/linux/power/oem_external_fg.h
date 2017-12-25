293fe925e7b3c3219c5d95bc3284c63dabe64736
Sultanxda <sultanxda@gmail.com>
2017-12-25 14:35:25 -0800

    power: Remove unneeded dash charge hacks
    I'm not sure what OnePlus was doing with this, but it's not needed to
    make dash charge work. Kill it.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- include/linux/power/oem_external_fg.h
+++ include/linux/power/oem_external_fg.h
@@ -109,7 +109,4 @@ void bq27541_information_unregister(struct external_battery_gauge *fast_chg);
 bool get_extern_fg_regist_done(void);
 bool get_extern_bq_present(void);
 
-extern bool dash_adapter_update_is_tx_gpio(unsigned long gpio_num);
-extern bool dash_adapter_update_is_rx_gpio(unsigned long gpio_num);
-
 #endif
